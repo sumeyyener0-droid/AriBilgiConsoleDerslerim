4ea6ca330bd63f98fdb3927217d4c84e0b9a283d
sümeyye <sumeyyener.0@gmail.com>
2026-01-27 21:34:35 +0300

    ..

---
+++ 260107_2_dowhile/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _260107_2_dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while döngüsü ilkin koşulaele alır,koşul doğru isedöngü içinde işleme alır.
+            //do while 1 defa  koşula bakmadan işlem yapar,sonra koşula bakar,koşul doğru isedöngü içinde kodlar tekrar işleme alınır
+            /*
+                        while(true)
+                        {
+                            //2. adım kodlar işleme alınır
+                        }
+
+                        do//do=> yap
+                        {
+                            //1.adım döngü kodlarının yazıldığı bu alan 1 kez işleme alınır 1 kez işleme alındıktan sonra
+                            // aşagıdaki gibi koşula bırakılır
+                            //kod alanı bittiğini aşagıdaki while yapısı ile anlaşılır
+                        } while (true);//2.adım koşul doğru ise tekrarkod alanına dönülür ve kodlar işleme alınır
+            */
+            int sayi = 1;
+            //while (sayi>0)//0>0=>false
+            //{
+            // Console.WwriteLine("sayi 0 dan büyüktür:");
+            //}
+            //*****************
+            //do while
+            Console.WriteLine("*************** do while********");
+
+            do
+            {
+                Console.WriteLine("sayı 0 dan buyuktur:");
+            }while(sayi>0);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 260127_dowhile_ornek1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _260127_dowhile_ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan alınan 5 sayıda büyük sayıyı hesaplayan yapıyı kodlayınız.
+            //kullanıcıdan negatif sayı alana kadar girilen pozitif sayıların toplamını ve adetini hesaplayan do while
+
+            //kullanıcıdan 1-100 arasında sayı girmesini isteyiniz,girilen sayi1-100 arasında değil ise
+
+            Console.WriteLine("1.sayıyı giriniz");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("2sayıyı giriniz");
+            int sayi2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("3.sayıyı giriniz");
+            int sayi3= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("4sayıyı giriniz");
+            int sayi4= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("5sayıyı giriniz");
+            int sayi5= Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
