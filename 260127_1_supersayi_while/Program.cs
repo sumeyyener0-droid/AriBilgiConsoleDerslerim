293bac9f6bc793cc8d7b6f9bc470d786ea9b9cce
sümeyye <sumeyyener.0@gmail.com>
2026-01-27 20:52:55 +0300

    while asal sayi süper sayi çözümleri

---
+++ 260127_1_supersayi_while/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Channels;
+
+namespace _260127_1_supersayi_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kendisi hariç bölünenlerin toplamı kendisini veren sayılara süpersayı denir.1-1000 arasındaki listele
+
+
+            long sayi = 0;
+
+
+            while (sayi > 1000)
+            {
+                long bolensayi = 1;
+                long toplam = 0;
+                while (sayi>bolensayi)
+                {
+                    if (sayi % bolensayi == 0)
+                    {
+                        toplam = toplam + bolensayi;
+                    }
+                    bolensayi++;
+                    if (toplam == sayi)
+                    {
+                        Console.WriteLine("supersayilar:" + sayi);
+                    }
+                    sayi++;
+                }
+                Console.ReadLine();
+               
+                        
+
+            }
+       
+
+        }
+
+    }
+}
---
+++ asal sayi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+
+namespace asal_sayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi2 = 2;
+            int bolensayi = 2;
+            int toplamasalsayilar = 0;
+            bool asalmi = true;
+            while (sayi2 > bolensayi)
+            {
+                asalmi = true;
+                if (sayi2 % bolensayi == 0)
+                {
+                    asalmi = false;
+                    break;
+                }
+                bolensayi++;
+            }
+            if (asalmi)
+            {
+                toplamasalsayilar = toplamasalsayilar + sayi2;
+                Console.WriteLine(sayi2 + ",");
+            }
+            sayi2++;
+        }
+       
+
+
+
+    }
+}
