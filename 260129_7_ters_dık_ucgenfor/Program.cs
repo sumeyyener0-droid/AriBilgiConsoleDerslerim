cd48e23a43a745a9c97d1b3111e2a61600b05920
sümeyye <sumeyyener.0@gmail.com>
2026-01-29 21:31:31 +0300

    ters yıldız

---
+++ 260129_7_ters_dık_ucgenfor/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _260129_7_ters_dık_ucgenfor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * for ile * karakteri kullanarak ters dik üçgen yapınız
+                            *
+                           **
+                         ****
+                        *****
+                       ******
+                      *******
+                     ********
+                    *********
+             */
+            string yildiz = "*";
+
+            for(int i=1; i<15; )
+            {
+                for(int k=15; k>i; k--)
+                {
+                    Console.Write("");
+                }
+                Console.WriteLine(yildiz);
+                yildiz += "*";
+            }
+
+        }
+    }
+}
