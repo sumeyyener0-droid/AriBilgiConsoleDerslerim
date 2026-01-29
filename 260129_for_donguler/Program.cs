50d9f6dc1589b549c05d56b1a1431764e31b77ed
sümeyye <sumeyyener.0@gmail.com>
2026-01-29 19:53:25 +0300

    ornek for

---
+++ 260129_for_donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _260129_for_donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for Döngüsü
+            //Genelde sayısal işlemlerde kullanılır ama her türlü kullanılabilir, kullanmaya bağlı
+
+            int sayac = 0;//while ve do while döngülerinde bu şekilde bir değişkene ihtiyaç duyuluyor ama for bu yapıyı kendi içinde getiriyor
+
+            while (sayac < 100)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+            //yukardaki while ile aşağıdaki for u karşılaştırarak kullanın
+            // for (1.Adım; 2.Adım; 3.Adım)
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + ",");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Tersten sayma for ile");
+            for (int i = 25; i > 10; i--)
+            {
+                Console.Write(i + ",");
+            }
+        }
+    }
+}
---
+++ 260129_for_ornek1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.NetworkInformation;
+
+namespace _260129_for_ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tek for ile 50-5000 11 ve 13 ün katları sayısı ve katları toplamlarını ayrı ayrı gösteriniz.
+
+            int adet11 = 0;
+            int adet13 = 0;
+            int toplam11 = 0;
+            int toplam13 = 0;
+            
+            for (int i = 50; i <= 5000; i++)
+            {
+                if (i %11== 0) 
+                {
+                   
+                    adet11++;
+                    toplam11 = toplam11 + i;
+                       
+                }
+                if(i%13==0)
+                {
+                    
+                    adet13++;
+                    toplam13 = toplam13 + i;
+
+                }
+
+
+            }
+            Console.WriteLine("11 in katları adeti(0) ve toplamı(1)" + adet11, toplam11);
+            Console.WriteLine("13 ün katları adeti(0) ve toplamı(1)" + adet13, toplam13);
+        }
+    }
+}
