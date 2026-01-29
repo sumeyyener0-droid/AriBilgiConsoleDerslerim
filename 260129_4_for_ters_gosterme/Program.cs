3a4fe6931227d16aa1d83e085b925648c8741c61
sümeyye <sumeyyener.0@gmail.com>
2026-01-29 20:48:46 +0300

    for tersden ornek

---
+++ 260129_4_for_ters_gosterme/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _260129_4_for_ters_gosterme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //0-50 arasında her  ekrana yazılacak sayı için büyükten küçüğe ekranda sayıları gösteriniz
+            
+            for (int i = 0; i <=50; i++)
+            {
+                Console.WriteLine(i+">=");
+
+                for(int k=i; k>=0; k--)
+                {
+                    Console.WriteLine(k+",");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
