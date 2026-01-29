104ac31e27f8f51bd1876cfb240acd7b21ab6d48
sümeyye <sumeyyener.0@gmail.com>
2026-01-29 21:17:16 +0300

    ..

---
+++ 260129_dık_ucgen_ters_sayi/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _260129_dık_ucgen_ters_sayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dik üçgenin karakter sayısı max alınacak şekilde max değerden 0 doğru aynı satırda
+
+            string yildiz = "*";
+            
+            for (int i=1; i<25; i++)
+            {
+                Console.Write(yildiz);
+                for (int k=i; k>=0; k--)
+                {
+                    Console.Write(k+",");
+                }
+
+                Console.WriteLine();
+
+                yildiz += "*";
+            }
+        }
+    }
+}
