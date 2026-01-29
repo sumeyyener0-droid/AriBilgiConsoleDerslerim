d34fc1497fdf6199e53da35299b770d1a632a9ce
sümeyye <sumeyyener.0@gmail.com>
2026-01-29 21:00:11 +0300

    şekil

---
+++ 260129_dik_ucgen/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _260129_dik_ucgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              * 
+              **
+              ***
+              ****
+              *****
+              ******
+              *******
+              */
+                string metin = "*";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(metin);
+                metin = metin + "*";
+
+
+            }
+        }
+    }
+}
