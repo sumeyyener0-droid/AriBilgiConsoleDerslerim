9805e75754600648f2a82f9e792a14fd2b579c6f
sümeyye <sumeyyener.0@gmail.com>
2026-01-30 20:44:23 +0300

    while toplama ornek

---
+++ 260130_2_diziler_ornek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _260130_2_diziler_ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 260130_3_dizi_while/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _260130_3_dizi_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullancıdan alınan 7 sayıyı aldıktan sonra ekranda gösterip toplamlarını,hesaplaint[
+            int[] sayilar = new int[7];
+            int elemansayisi = sayilar.Count();
+            int toplam = 0;
+            int sayac = 0;
+            while(sayac<elemansayisi)
+            {
+                Console.WriteLine(sayac+1+". sayıyı giriniz");
+                sayilar[sayac] = Convert.ToInt16(Console.ReadLine());
+                toplam += sayilar[sayac];
+                sayac++;
+            }
+            int i = 0;
+            Console.WriteLine("girilen sayılar");
+
+            do
+            {
+                Console.WriteLine(i + i);
+                i++;
+            } while (i < elemansayisi);
+            Console.WriteLine("girilen sayilar toplamı"+toplam);
+         
+        }
+    }
+}
