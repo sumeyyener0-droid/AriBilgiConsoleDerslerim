5033d17223a6ce2ed44e1299baaa8cfcaaa9dee5
sümeyye <sumeyyener.0@gmail.com>
2026-01-30 20:56:15 +0300

    ornek4

---
+++ 260130_4_diziler_ornek3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _260130_4_diziler_ornek3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dizi = new double[6];
+            //elemanı ekleyip,eklenen elemanları ekranda gösterniz
+
+            dizi[0] = 7;
+            dizi[1] = 8;
+            dizi[2] = 9;
+            dizi[3] = 10;
+            dizi[4] = 11;
+            dizi[5] = 12;
+
+            //kullanıcıdan kendisi isterse
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(i+1+".elemanı:");
+                dizi[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            // bu üstteki sorunun cevap
+
+            for (int i = 0; i <dizi.Length; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+        }
+    }
+}
