3712e8ad2b7fb34af95075f0f8fa8938884c4d74
sümeyye <sumeyyener.0@gmail.com>
2026-02-06 21:28:19 +0300

    return voıd metod işlendi

---
+++ 260206_4_method_ornekler/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _260206_4_method_ornekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EkranaYaZ("1. sayıyı giriniz:");
+            int sayi1 = IntYap();
+            //int sayi1=Convert.ToInt32(Console.ReadLine());
+            //int sayi1=Convert.ToInt32(oku());
+            EkranaYaZ("Girilen sayı:" + sayi1);
+        }
+        static void EkranaYaZ(string metin)
+        {
+            Console.WriteLine(metin);
+        }
+
+        static string Oku()
+        {
+            return Console.ReadLine();
+        }
+        static int IntYap()
+            {
+            return Convert.ToInt32(Oku());
+        }
+    }
+}
