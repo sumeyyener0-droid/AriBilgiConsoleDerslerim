73c802c716ed9b2cb12dfe5cb708c739a93882e8
sümeyye <sumeyyener.0@gmail.com>
2026-02-06 20:53:24 +0300

    voıd

---
+++ 260206_2_deger_dondurmeyen_voıd_method/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _260206_2_deger_dondurmeyen_voıd_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //değer döndürmeyen void method , method adından önce void ifadesi içeriyorsa değer döndürmeyen method olur.
+
+            Cizgi();
+            Console.WriteLine("adınız:");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("soyadınız:");
+            string soyad = Console.ReadLine();
+            AdSoyadBuyukHarf(ad, soyad);
+        }
+        static void Cizgi()
+        {
+            Console.WriteLine("------------------");
+        }
+
+        static void AdSoyadBuyukHarf(string ad, string soyad);
+        {
+        Console.WriteLine("ad:{0} ve soyad{1}"ad.ToUpper(),soyad.ToUpper());
+
+        }
+
+    }
+}
