8f7563e3e5c6cc8e7c7f12423e929d7d96b7e17c
sumeyyener0-droid <sumeyyener.0@gmail.com>
2026-03-03 17:45:39 +0300

    bankamatk

---
+++ bankamtik_proje/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace bankamtik_proje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void BaslangicMenusu()
+        {
+            Console.WriteLine("1-Kartlı İşlem");
+            Console.WriteLine("2-Kartsız İşlem");
+            string secim =Console.ReadLine();
+            if()
+        }
+
+    }
+}
